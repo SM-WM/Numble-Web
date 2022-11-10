cfb4748415e8107300d8e43d170f1478727bd41e
Joram Bosire <jbosire@vassar.edu>
2022-11-10 11:14:22 -0500

    Initialize files in backend

---
+++ Backend/numble-schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users(
+    id              SERIAL PRIMARY KEY,
+    password        TEXT NOT NULL,
+    firstName      TEXT NOT NULL,
+    lastName       TEXT NOT NULL,
+    email           TEXT NOT NULL UNIQUE CHECK (POSITION('@' IN email) > 1),
+    createdAt      TIMESTAMP NOT NULL DEFAULT NOW(),
+    updatedAt      TIMESTAMP NOT NULL DEFAULT NOW()
+);
---
+++ Backend/numble.sql
@@ -0,0 +1,10 @@
+\echo "Delete and recreate numble db?"
+\prompt "Return for yes or control-C to cancel >" answer
+
+DROP DATABASE numble;
+CREATE DATABASE numble;
+\connect numble;
+
+\i numble-schema.sql
+\i numble-seed.sql
+
